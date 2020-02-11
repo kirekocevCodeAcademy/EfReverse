0994f7581fb76aa5d274232878d00efbc459aa2e
kirekocevCodeAcademy <kirekocevCodeAcademy@gmail.com>
2020-02-11 09:00:36 +0100

    Add project files.

---
+++ TestReverseEf/Stores.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestReverseEf
+{
+    public partial class Stores
+    {
+        public Stores()
+        {
+            Discounts = new HashSet<Discounts>();
+            Sales = new HashSet<Sales>();
+        }
+
+        public string StorId { get; set; }
+        public string StorName { get; set; }
+        public string StorAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+        public virtual ICollection<Discounts> Discounts { get; set; }
+        public virtual ICollection<Sales> Sales { get; set; }
+    }
+}
